3476f5ebbd163a30cbc2c353bc931587ac2dc82e
Uran_Hajrizaj <uranhajrizaj@outlook.com>
2022-06-21 10:22:15 +0200

    changed

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 let  letter=prompt("Enter a letter");
- users.filter((user) => user.name.includes(letter)).map((user) => console.log(`Hi there, ${user.name}!`));
+ let nameofUsers =users.filter((user) => user.name.toLowerCase().includes(letter.toLowerCase()));
+ let i=0;
+ let intervalId=setInterval(() => {
+    let user=nameofUsers[i];
+    if(user){
+    console.log(`Hi ${user.name}`);
+    i++;}
+    else{
+        clearInterval(intervalId);
+    }
+ } , 2000);
+ 
  //   - Prompt the user for a user ID. Using the value, find all the todos that are Incomplete and console.log them.
  let  number=Number(prompt("Enter your id"));
- todos.filter((user) => user.id === number).map((todo) => console.log(`Your todos, ${todo.title}!`));
+ 
