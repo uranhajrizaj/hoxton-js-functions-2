9727cc0ecfd634c7404149797f670ce2d24303ba
Uran_Hajrizaj <uranhajrizaj@outlook.com>
2022-06-21 09:31:22 +0200

    changed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let  letter=prompt("Enter a letter");
-
-  users.filter((user) => user.name.includes(letter)).map((user) => console.log(`Hi there, ${user.name}!`));
+ users.filter((user) => user.name.includes(letter)).map((user) => console.log(`Hi there, ${user.name}!`));
+ //   - Prompt the user for a user ID. Using the value, find all the todos that are Incomplete and console.log them.
+ let  number=Number(prompt("Enter your id"));
+ todos.filter((user) => user.id === number).map((todo) => console.log(`Your todos, ${todo.title}!`));
